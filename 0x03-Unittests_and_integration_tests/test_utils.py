1425dffaa69b602e3001c6bd25aef62382beda98
Bassem Gamal <bassemgamalosman12@gmail.com>
2024-09-13 19:27:46 +0300

    Task 1: Parameterize a unit test

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -24,3 +24,16 @@ class TestAccessNestedMap(unittest.TestCase):
     ) -> None:
         """ Test access_nested_map is working """
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        (dict(), ("a",), "'a'"),
+        ({"a": 1}, ("a", "b"), "'b'"),
+    ])
+    def test_access_nested_map_exception(
+        self, nested_map: Mapping[str, Any],
+        path: Sequence[str], expected_msg: str
+    ) -> None:
+        """ Test access_nested_map with raise """
+        with self.assertRaises(KeyError) as e:
+            access_nested_map(nested_map, path)
+        self.assertEqual(str(e.exception), expected_msg)
